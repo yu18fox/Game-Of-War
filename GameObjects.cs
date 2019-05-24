bdbf8f70d153a81038c8ec0d65f5d94a3f84f899
yu18fox <yu18fox@aol.com>
2019-05-24 11:33:35 -0400

    Add files via upload

---
+++ GameObjects.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfWar
+{
+
+    //Enums
+    public enum Suite
+    {
+        DIAMONDS, CLUBS, HEARTS, SPADES
+    }
+
+    public enum Rank
+    {
+        TWO = 2, THREE = 3, FOUR = 4, FIVE = 5, SIX = 6, SEVEN = 7, EIGHT = 8, NINE = 9, TEN = 10, JACK = 11, QUEEN = 12, KING = 13, ACE = 14
+    }
+
+    public static class Extensions
+    {
+        public static T GetEnumValue<T>(string str) where T : struct, IConvertible
+        {
+            if (!typeof(T).IsEnum)
+            {
+                throw new Exception("T must be an Enumeration type.");
+            }
+            T val = ((T[])Enum.GetValues(typeof(T)))[0];
+            if (!string.IsNullOrEmpty(str))
+            {
+                foreach (T enumValue in (T[])Enum.GetValues(typeof(T)))
+                {
+                    if (enumValue.ToString().ToUpper().Equals(str.ToUpper()))
+                    {
+                        val = enumValue;
+                        break;
+                    }
+                }
+            }
+
+            return val;
+        }
+
+        public static T GetEnumValue<T>(int intValue) where T : struct, IConvertible
+        {
+            if (!typeof(T).IsEnum)
+            {
+                throw new Exception("T must be an Enumeration type.");
+            }
+            T val = ((T[])Enum.GetValues(typeof(T)))[0];
+
+            foreach (T enumValue in (T[])Enum.GetValues(typeof(T)))
+            {
+                if (Convert.ToInt32(enumValue).Equals(intValue))
+                {
+                    val = enumValue;
+                    break;
+                }
+            }
+            return val;
+        }
+
+    }
+    //Domain Objects
+    public class Card
+    {
+        public Rank Rank { get; set; }
+        public Suite Suite { get; set; }
+    }
+    public class Dealer
+    {
+        private Stack<Card> Deck;
+        public Dealer()
+        {
+            GetNewDeck();
+        }
+
+        public void GetNewDeck()
+        {
+            Deck = new Stack<Card>();
+            for (int suite = 1; suite < 5; suite++)
+            {
+                for (int rank = 2; rank < 15; rank++)
+                {
+                    Deck.Push(new Card()
+                    {
+                        Rank = Extensions.GetEnumValue<Rank>(rank),
+                        Suite = Extensions.GetEnumValue<Suite>(suite)
+                    });
+                }
+            }
+        }
+
+        public void DealCardsToPlayers(Player[] players)
+        {
+            Deck = Shuffle(Deck);
+            while (Deck.Any())
+            {
+                foreach (var player in players)
+                {
+                    if (Deck.Count > 0)
+                    {
+                        player.Hand.Enqueue(Deck.Pop());
+                    }
+                }
+            }
+        }
+        public Queue<Card> Shuffle(Queue<Card> deck)
+        {
+            Stack<Card> shuffledStack = Shuffle(new Stack<Card>(deck.ToList()));
+            return new Queue<Card>(shuffledStack.ToList());            
+        }
+        public Stack<Card> Shuffle(Stack<Card> deck)
+        {
+            var random = new Random();
+            int numberOfTimesToShuffle = random.Next(10, 20);
+
+            for (; numberOfTimesToShuffle != 0; numberOfTimesToShuffle--)
+            {
+                //split the deck into 2 halfs
+                Stack<Card> LeftDeck = new Stack<Card>();
+                Stack<Card> RightDeck = new Stack<Card>();
+                int halfDeckSize = (int)(deck.Count / 2);
+
+                for (int c = 0; c <= halfDeckSize; c++)
+                {
+                    if (c < halfDeckSize)
+                    {
+                        LeftDeck.Push(deck.Pop());
+                    }                   
+                }
+                for(int r = deck.Count; r!= 0; r--)
+                {
+                    RightDeck.Push(deck.Pop());
+                }
+
+                while (LeftDeck.Any() || RightDeck.Any())
+                {
+                    int randomNumberLeft = random.Next(1, 4);
+                    int randomNumberRight = random.Next(1, 4);
+                    for (; randomNumberLeft != 0; randomNumberLeft--)
+                    {
+                        if (LeftDeck.Any())
+                        {
+                            deck.Push(LeftDeck.Pop());
+                        }
+                    }
+                    for(; randomNumberRight != 0; randomNumberRight--)
+                    {
+                        if(RightDeck.Any())
+                        {
+                            deck.Push(RightDeck.Pop());
+                        }
+                    }
+                }
+            }
+
+            return deck;
+        }
+
+        public void PrintDeckToScreen()
+        {
+            Deck = Shuffle(Deck);
+            foreach(var card in Deck)
+            {
+                Console.Write(String.Format("{0} {1} ", card.Rank, card.Suite));
+            }
+        }
+    }
+    public class Player
+    {
+        public Player()
+        {
+            Hand = new Queue<Card>();
+        }
+        public string PlayerName { get; set; }
+        public Queue<Card> Hand { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfWar
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var players = new Player[2]
+            {
+                new Player()
+                {
+                    PlayerName = "Player 1"
+                },
+                new Player()
+                {
+                    PlayerName = "Player 2"
+                }
+            };
+
+            int playerShuffleInterval = 0;
+            var dealer = new Dealer();
+            dealer.GetNewDeck();
+            dealer.DealCardsToPlayers(players);
+
+            while (!players.Any(a => a.Hand.Count == 0))
+            {
+                playerShuffleInterval++;
+                if (playerShuffleInterval > 20)
+                {
+                    foreach (var player in players)
+                    {
+                        player.Hand = dealer.Shuffle(player.Hand);
+                    }
+                    playerShuffleInterval = 0;
+                }
+                CalculateWinner(players, null);
+                Console.WriteLine("Press a key to battle");
+                Console.ReadLine();
+
+            }
+            Console.WriteLine(String.Format("The winner is {0}!", players.Single(a => a.Hand.Any()).PlayerName));
+            Console.ReadLine();
+        }
+
+        public static void CalculateWinner(Player[] players, List<Card> warCards)
+        {
+            Player player1 = players[0];
+            Player player2 = players[1];
+            Console.WriteLine(String.Format("Player1 Card Count: {0}", player1.Hand.Count));
+            Console.WriteLine(String.Format("Player2 Card Count: {0}", player2.Hand.Count));
+
+            Card nextCardPlayer1 = player1.Hand.Dequeue();
+            Card nextCardPlayer2 = player2.Hand.Dequeue();
+            Console.WriteLine(String.Format("{0}: {1} {2} | {3}: {4} {5}", player1.PlayerName, nextCardPlayer1.Rank, nextCardPlayer1.Suite, player2.PlayerName, nextCardPlayer2.Rank, nextCardPlayer2.Suite));
+
+            if (nextCardPlayer1.Rank > nextCardPlayer2.Rank)
+            {
+                //player1 wins this hand
+                Console.WriteLine(String.Format("{0} wins this hand", player1.PlayerName));
+                player1.Hand.Enqueue(nextCardPlayer1);
+                player1.Hand.Enqueue(nextCardPlayer2);
+                if (warCards != null)
+                {
+                    foreach (var card in warCards)
+                    {
+                        player1.Hand.Enqueue(card);
+                    }
+                }
+            }
+            else if (nextCardPlayer1.Rank < nextCardPlayer2.Rank)
+            {
+                //player2 winds this hand
+                Console.WriteLine(String.Format("{0} wins this hand", player2.PlayerName));
+                player2.Hand.Enqueue(nextCardPlayer1);
+                player2.Hand.Enqueue(nextCardPlayer2);
+                if (warCards != null)
+                {
+                    foreach (var card in warCards)
+                    {
+                        player2.Hand.Enqueue(card);
+                    }
+                }
+            }
+            else
+            {
+                //They are equal, I declare war
+                Console.WriteLine("I DECLARE WAR!");
+                if (warCards == null)
+                {
+                    warCards = new List<Card>() { };
+                }
+                warCards.Add(nextCardPlayer1);
+                warCards.Add(nextCardPlayer2);
+                GoToWar(warCards, players);
+            }
+        }
+        public static void GoToWar(List<Card> warCards, Player[] players)
+        {
+            if (warCards == null)
+            {
+                warCards = new List<Card>();
+            }
+            foreach (var p in players)
+            {
+                for (int p1 = 3; p.Hand.Count > 1 && p1 > 0; p1--)
+                {
+                    warCards.Add(p.Hand.Dequeue());
+                }
+            }
+            if (players.Any(a => a.Hand.Count == 0))
+                {
+                throw new Exception("Player had no cards left ");
+            }
+            CalculateWinner(players, warCards);
+        }
+    }
+}
+
